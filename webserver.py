10fda769a48fec8d949280da019404ae6470535c
Sklorby <140915943+Sklorby@users.noreply.github.com>
2024-08-19 17:51:59 -0500

    webserver.py

--- webserver.py
+++ webserver.py
@@ -8,7 +8,7 @@ def home():
     return "Discord bot ok"
 
 def run():
-    app.run(host="0.0.0.0", port=os.environ["PORT"])
+    app.run(host='0.0.0.0', port=os.environ["PORT"])
 
 def keep_alive():
     t = Thread(target=run)
