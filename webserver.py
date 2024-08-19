03ac438a285cf3bec8b0b260305059f0ce70454c
Sklorby <140915943+Sklorby@users.noreply.github.com>
2024-08-19 17:56:02 -0500

    webserver.py

--- webserver.py
+++ webserver.py
@@ -8,7 +8,9 @@ def home():
     return "Discord bot ok"
 
 def run():
-    app.run(host='0.0.0.0', port=os.environ["PORT"])
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    print(f"Starting Flask app on port {port}")
+    app.run(host='0.0.0.0', port)
 
 def keep_alive():
     t = Thread(target=run)
