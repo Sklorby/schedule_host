074a514a997e81c790ae781a3726cdd092cd6fca
Sklorby <140915943+Sklorby@users.noreply.github.com>
2024-08-19 16:25:55 -0500

    data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -169,7 +169,7 @@ def add_student(f: str, l: str) -> str:
     response = requests.post(url=CREATE_PAGE, json=data, headers=headers)
     id = response.json()['id']
 
-    open('student_IDs.txt', mode='a') as file:
+    with open('student_IDs.txt', mode='a') as file:
         file.write(f'{first} {last}={id}\n')
         file.flush()
     
