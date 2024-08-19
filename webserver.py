6dd45dbcba29eb387459878cf3edc66cf7e42084
Sklorby <140915943+Sklorby@users.noreply.github.com>
2024-08-19 18:36:31 -0500

    webserver.py

--- webserver.py
+++ webserver.py
@@ -10,7 +10,7 @@ def home():
 def run():
     port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
     print(f"Starting Flask app on port {port}")
-    app.run(debug=True, host='0.0.0.0', port)
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
